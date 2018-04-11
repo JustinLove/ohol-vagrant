39d605a8b8f5582ec4ec1805e85880cbaf64e660
Justin Love <git@JustinLove.name>
2018-04-11 13:21:53 -0500

    setting updates

--- install.sh
+++ install.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
 sudo apt-get update
-sudo apt-get install git g++ imagemagick xclip make libsdl1.2-dev libglu1-mesa-dev libgl1-mesa-dev
+sudo apt-get install -y git g++ imagemagick xclip make libsdl1.2-dev libglu1-mesa-dev libgl1-mesa-dev
 cd /ohol/
 ./pullAndBuildLatest 
 ./pullAndBuildTestSystem.sh 
 cd OneLife/server/
-echo 0 > /ohol/OneLife/server/settings/allowBugReports.ini
-echo 0 > /ohol/OneLife/server/settings/requireTicketServerCheck.ini
-echo 0 > /ohol/OneLife/server/settings/useStatsServer.ini
+echo 0 > settings/requireTicketServerCheck.ini
+echo 0 > settings/useStatsServer.ini
+echo 0 > settings/allowBugReports.ini
+echo 0 > settings/apocalypsePossible.ini
+> settings/reflectorURL.ini
+echo 1 > settings/forceEveLocation.ini
