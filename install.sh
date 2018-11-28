6ca942824ce2c8d55b31a5ca8eab692c447ce72a
Justin Love <git@JustinLove.name>
2018-11-28 12:09:39 -0500

    setup gui desktop for client dev

--- install.sh
+++ install.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
 
 sudo apt-get update
+
+# OHOL dev
 sudo apt-get install -y git g++ imagemagick xclip make libsdl1.2-dev libglu1-mesa-dev libgl1-mesa-dev
+
+# VM GUI desktop
+sudo apt-get install -y xfce4 virtualbox-guest-dkms virtualbox-guest-utils virtualbox-guest-x11
+sudo VBoxClient --clipboard
+sudo VBoxClient --draganddrop
+sudo VBoxClient --display
+sudo VBoxClient --checkhostversion
+sudo VBoxClient --seamless
+
 cd /ohol/
 ./fetch_latest_tagged.sh
 ./build_server.sh
